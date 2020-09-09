2ae1578b17555220197162d72dcd7726d821094c
jarrett <gjb@qinqinxiaobao.com>
2020-09-09 18:45:22 +0800

    init

---
+++ build/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.ts",
+    output: {
+        filename: "main.js",
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./dist",
+        stats: "errors-only",
+        compress: false,
+        host: "localhost",
+        port: 8888
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ["./dist"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/template/index.html"
+        })
+    ]
+}
